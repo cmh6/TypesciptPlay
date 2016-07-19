93f609e12e510eadd76b0a047272e3768a4f7fc8
Chris Hines <chris.hines@perficient.com>
2016-07-19 11:20:28 -0400

    Test

---
+++ hello.ts
@@ -0,0 +1,26 @@
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+
+let label = document.createElement('div');
+label.textContent = "My Label";
+
+button.onclick = function() {
+	label.textContent = greeter.greet();
+    alert(greeter.greet());
+
+}
+
+document.body.appendChild(button);
+document.body.appendChild(label);
